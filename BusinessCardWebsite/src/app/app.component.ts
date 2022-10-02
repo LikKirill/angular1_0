f27116e4cec0bad7493a1108f1422eac71eb463f
Dare7890 <vladfetisov2011@yandex.ru>
2022-10-02 13:01:40 +0300

    [fetisov_va] feat: Добавление кнопки Старт/Стоп

--- BusinessCardWebsite/src/app/app.component.ts
+++ BusinessCardWebsite/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+type ButtonTextType = 'Старт' | 'Стоп';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  private a = 5;
+  public buttonText: ButtonTextType = 'Старт';
+
+  public changeButtonText() {
+    this.buttonText = this.buttonText === 'Старт' ? 'Стоп' : 'Старт';
+  }
 }

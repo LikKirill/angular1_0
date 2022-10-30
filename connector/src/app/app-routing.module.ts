5861a21f328dbe2898d647b46e5efe7c9aa1d827
Aura Philomena <1.katrin.1@mail.ru>
2022-10-30 20:55:12 +0300

    fix lesson-3

--- connector/src/app/app-routing.module.ts
+++ connector/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from "./app.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', title: 'Edit', component:AppComponent },
+  { path:'music', title: 'Listen', component:AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
